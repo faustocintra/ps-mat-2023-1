fd75921459c81bb53f7f8e945fa414b09550e2c5
Fausto G. Cintra <professor@faustocintra.com.br>
2023-02-16 14:03:58 +0000

    (16/02) Criação do primeiro model e migration

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+// Carrega as variáveis de ambiente do arquivo
+// .env para a aplicação
+require('dotenv').config()
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -7,6 +11,19 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// Conexão ao BD ------------------------------------------
+const db = require('./models')
+
+try {
+  db.sequelize.authenticate()
+  console.log('SEQUELIZE: connection has been established successfully.')
+}
+catch(error) {
+  console.error('* SEQUELIZE: unable to connect to the database: ', error)
+  process.exit(1)     // Encerra o servidor com erro
+}
+// ---------------------------------------------------------
 
 app.use(logger('dev'));
 app.use(express.json());
