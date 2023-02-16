6a7586f3f7767056bd3236e43dd0528e07a80f50
Heitor Saqueto <54643371+Heitor-Saqueto@users.noreply.github.com>
2023-02-16 14:05:02 +0000

    (16/02)Criação do primeiro model e migration

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require ('dotenv'). config();
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -7,7 +9,15 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+const db = require('./models')
+try {
+    db.sequelize.authenticate()
+    console.log('SEQUELIZE: conection has been established succesfully.')
+}
+catch(error){
+    console.error('*SEQUELIZE: unable to connect to the database:',error)
+    process.exit(1) //encerra o servidor com erro
+}
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
